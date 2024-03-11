8b58b6b875394a4454746a9ecdc376c2296222c6
Sardonyxyu <sardonyxyu@qq.com>
2024-03-11 18:05:04 +0800

    修改Banner广告低版本显示过高问题（解决）

--- src/main/java/com/yingyongduoduo/ad/utils/ScreenUtils.java
+++ src/main/java/com/yingyongduoduo/ad/utils/ScreenUtils.java
@@ -34,6 +34,14 @@ public class ScreenUtils {
     }
 
     /**
+     * 根据手机的分辨率从 px(像素) 的单位 转成为 dp
+     */
+    public static int px2dp(Context context, float pxValue) {
+        final float scale = context.getResources().getDisplayMetrics().density;
+        return (int) (pxValue / scale + 0.5f);
+    }
+
+    /**
      * 获得状态栏的高度
      *
      * @param context
